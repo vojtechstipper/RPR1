a8b5a055173e4713eba67f076a6c436c242e7a0a
Vojtěch Stipper <vojtech.stipper@itixo.com>
2023-11-07 13:25:22 +0100

    Added allergensConroller and query for listing all allergens

---
+++ ReservationSystemBE/ReservationSystemBE/Application/Allergens/Queries/AllergenDto.cs
@@ -0,0 +1,17 @@
+﻿namespace ReservationSystemBE.Application.Allergens.Queries;
+
+public class AllergenDto
+{
+    public AllergenDto(string id, int code, string name, string description)
+    {
+        Id = id;
+        Code = code;
+        Name = name;
+        Description = description;
+    }
+
+    public string Id { get; set; }
+    public int Code { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}

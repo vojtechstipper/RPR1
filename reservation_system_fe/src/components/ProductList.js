2aa394f264264425a996a9d48ad186adbb7b60c8
Vojtěch Stipper <vojtech.stipper@itixo.com>
2023-11-18 19:35:18 +0100

    added grid

--- reservation_system_fe/src/components/ProductList.js
+++ reservation_system_fe/src/components/ProductList.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import api from '../services/api';
+import {getProductsGroupped} from '../services/apiService';
 import ProductCard from './ProductCard';
+import Grid from '@mui/material/Grid';
 
 function ProductList() {
-  const [products, setProducts] = useState([]);
+  const [productTypesWithProducts, setProducts] = useState([]);
 
   useEffect(() => {
     async function fetchProducts() {
       try {
-        const response = await api.get('/product/list');
-        setProducts(response.data);
+        const response = await getProductsGroupped();
+        setProducts(response.result);
         console.log("products")
-        console.log(products);
+        console.log(response.result);
       } catch (error) {
         console.error('Chyba při načítání produktů:', error);
       }
@@ -20,13 +21,25 @@ function ProductList() {
     fetchProducts();
   }, []);
 
+  
   return (
     <div>
-    {products.map((product) => (
-<ProductCard name={product.name} description={product.description} allergens={product.allergens}></ProductCard>))}
-  
+      {productTypesWithProducts.map((product) => (
+        <div>
+          <h1>{product.productType.name}</h1>
+          <Grid container >
+          {product.products.map((item) => (
+            <ProductCard 
+              name={item.name}
+              description={item.description}
+              allergens={item.allergens}
+            ></ProductCard>
+          ))}
 
- </div>
+          </Grid>
+        </div>
+      ))}
+    </div>
   );
 };
 

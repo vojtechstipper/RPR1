9c65f8ff037711fd514f5d65ec7ea31d3e9513f4
Daniel Chlopcik <daniel.chlopcik@storaenso.com>
2023-11-19 11:35:55 +0100

    Revert "Add endpoint on add allergens seeds"
    This reverts commit a0de18eb44813b3511029e1e4212941ee86d61a8.

--- ReservationSystemBE/ReservationSystemBE/Application/Test/SeedDtos.cs
+++ ReservationSystemBE/ReservationSystemBE/Application/Test/SeedDtos.cs
@@ -24,16 +24,4 @@ public class SeedDtos
     {
         public List<ProductTypeSeedDto> Items { get; set; }
     }
-
-    public class AllergensDto
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public int Code { get; set; }
-    }
-
-    public class SeedObjectAllergens
-    {
-        public List<AllergensDto> Allergens { get; set; }
-    }
 }

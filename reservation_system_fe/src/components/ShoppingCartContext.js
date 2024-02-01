228049bb066c8c9509358227da25c17ae5b74647
Daniel Chlopcik <daniel.chlopcik@storaenso.com>
2024-02-01 20:57:06 +0100

    add save state cart into localStorage

--- reservation_system_fe/src/components/ShoppingCartContext.js
+++ reservation_system_fe/src/components/ShoppingCartContext.js
@@ -1,10 +1,16 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 
 const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
-  const [cartData, setCartData] = useState([]);
-  const [cartOtherData, setCartOtherData] = useState();
+  const [cartData, setCartData] = useState(() => {
+    const localData = localStorage.getItem("cartData");
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("cartData", JSON.stringify(cartData));
+  }, [cartData]);
 
   const addToCart = (item) => {
     setCartData((prevData) => [...prevData, item]);

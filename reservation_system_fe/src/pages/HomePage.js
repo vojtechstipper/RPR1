66627956ecc74f23cfb8bd661e0657a2638ed728
Daniel Chlopcik <daniel.chlopcik@storaenso.com>
2024-02-24 22:14:46 +0100

    refactor homepage

--- reservation_system_fe/src/pages/HomePage.js
+++ reservation_system_fe/src/pages/HomePage.js
@@ -1,5 +1,6 @@
-import Navbar from "../components/Navbar";
 import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import {
   Box,
   Typography,
@@ -8,15 +9,18 @@ import {
   Paper,
   CardMedia,
   IconButton,
+  useTheme,
+  useMediaQuery,
 } from "@mui/material";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import { useNavigate } from "react-router-dom";
-import Footer from "../components/Footer";
 import Coverphoto from "../static/img/coverphoto.jpg";
 
 function HomePage() {
   let navigate = useNavigate();
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down("sm"));
 
   const goToMenu = () => {
     navigate("/menu");
@@ -29,92 +33,76 @@ function HomePage() {
         container
         spacing={2}
         alignItems="center"
-        display={"flex"}
-        style={{ minHeight: "50vh" }}
+        justifyContent="center"
+        sx={{ minHeight: "100vh", padding: matches ? 2 : 8 }}
       >
-        <Grid item xs={12} sm={6}>
-          <Box sx={{ p: 2, textAlign: "left", paddingLeft: "200px" }}>
-            <Typography variant="h4" gutterBottom fontWeight="bold">
-              Vítá vás City Campus Coffee
-            </Typography>
-            <Typography variant="subtitle1" sx={{ mb: 2 }}>
-              Jsme kavárna/bistro/školní bufet/bar a mnoho dalšího. Jsme přesně
-              to co zrovna potřebujete (provizorní text).
-            </Typography>
+        <Grid item xs={12} md={6} sx={{ p: 2 }}>
+          <Typography
+            variant="h4"
+            gutterBottom
+            fontWeight="bold"
+            textAlign={matches ? "center" : "left"}
+          >
+            Vítá vás City Campus Coffee
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            sx={{ mb: 2, textAlign: matches ? "center" : "left" }}
+          >
+            Jsme kavárna/bistro/školní bufet/bar a mnoho dalšího. Jsme přesně
+            to, co zrovna potřebujete (provizorní text).
+          </Typography>
+          <Box
+            display="flex"
+            justifyContent={matches ? "center" : "flex-start"}
+          >
             <Button
-              variant="h2"
-              sx={{
-                backgroundColor: "#e0e0e0",
-                color: "#000",
-                borderRadius: 2,
-                textTransform: "none",
-                fontWeight: "200",
-                boxShadow:
-                  "0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)",
-                "&:hover": {
-                  backgroundColor: "#d3d3d3",
-                  boxShadow:
-                    "0px 4px 5px -2px rgba(0,0,0,0.2), 0px 7px 10px 1px rgba(0,0,0,0.14), 0px 2px 16px 1px rgba(0,0,0,0.12)",
-                },
-              }}
+              variant="contained"
               onClick={goToMenu}
+              sx={{ textTransform: "none" }}
             >
               Přejít na nabídku
             </Button>
           </Box>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              height: "600px",
-              width: "100%",
-            }}
+        <Grid
+          item
+          xs={12}
+          md={6}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Paper elevation={4} sx={{ maxWidth: "80%", overflow: "hidden" }}>
+            <CardMedia
+              component="img"
+              image={Coverphoto}
+              alt="City Campus Coffee"
+            />
+          </Paper>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sx={{ display: "flex", justifyContent: "center", mt: 4 }}
+        >
+          <IconButton
+            component="a"
+            href="https://www.instagram.com/citycampuscoffee/?igshid=NGVhN2U2NjQ0Yg%3D%3D"
+            target="_blank"
           >
-            <Paper
-              elevation={4}
-              sx={{
-                maxWidth: "80%",
-                height: "80%",
-                overflow: "hidden",
-              }}
-            >
-              <CardMedia
-                component="img"
-                image={Coverphoto}
-                alt="City Campus Coffee"
-                sx={{ height: "auto", width: "100%" }}
-              />
-            </Paper>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                flexGrow: 0,
-                borderRadius: "10px",
-                padding: "10px",
-              }}
-            >
-              <IconButton
-                component="a"
-                href="https://www.instagram.com/citycampuscoffee/?igshid=NGVhN2U2NjQ0Yg%3D%3D"
-                target="_blank"
-                sx={{ color: "black" }}
-              >
-                <InstagramIcon fontSize="large" />
-              </IconButton>
-              <IconButton
-                component="a"
-                href="https://www.facebook.com/profile.php?id=61550106100216"
-                target="_blank"
-                sx={{ color: "black", mt: "0px" }}
-              >
-                <FacebookIcon fontSize="large" />
-              </IconButton>
-            </Box>
-          </Box>
+            <InstagramIcon fontSize="large" />
+          </IconButton>
+          <IconButton
+            component="a"
+            href="https://www.facebook.com/profile.php?id=61550106100216"
+            target="_blank"
+            sx={{ ml: 2 }}
+          >
+            <FacebookIcon fontSize="large" />
+          </IconButton>
         </Grid>
       </Grid>
       <Footer />

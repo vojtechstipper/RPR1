ba8bfb8b575586254e106e1948567ebfb5e344e7
Vojtěch Stipper <vojtech.stipper@itixo.com>
2024-03-11 19:47:19 +0100

    added endpoint for fetching users and added pagination

---
+++ ReservationSystemBE/ReservationSystemBE/Application/Users/Queries/PaginatedUsersQuery.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ReservationSystem.Domain.Users;
+using ReservationSystem.Shared.DTO;
+using ReservationSystemBE.Infrastructure.Persistence;
+
+namespace ReservationSystemBE.Application.Users.Queries;
+
+public class PaginatedUsersQuery : IRequest<PaginatedResult<UserDto>>
+{
+    public int Page { get; set; } = 1;
+    public int Count { get; set; } = 20;
+}
+
+public class PaginatedUsersQueryValidator : AbstractValidator<PaginatedUsersQuery>
+{
+    public PaginatedUsersQueryValidator()
+    {
+        RuleFor(x => x.Page).GreaterThan(0);
+        RuleFor(x => x.Count).GreaterThan(0);
+    }
+}
+
+public class UserDto
+{
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+    public string Email { get; set; }
+    public bool IsVerified { get; set; }
+    public UserRole Role { get; set; }
+}
+
+public class PaginatedUsersQueryHandler : IRequestHandler<PaginatedUsersQuery, PaginatedResult<UserDto>>
+{
+    private readonly ReservationSystemDbContext _context;
+    private readonly IMapper _mapper;
+
+    public PaginatedUsersQueryHandler(ReservationSystemDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedResult<UserDto>> Handle(PaginatedUsersQuery request, CancellationToken cancellationToken)
+    {
+        var totalCount = await _context.Users.CountAsync();
+        var users = await _mapper.ProjectTo<UserDto>(_context.Users.Skip((request.Page - 1) * request.Count).Take(request.Count)).ToListAsync();
+        return new PaginatedResult<UserDto>() { CurrentPage = request.Page, Data = users, TotalCount = totalCount };
+    }
+}
---
+++ ReservationSystemBE/Shared/DTO/PaginatedResult.cs
@@ -0,0 +1,9 @@
+﻿namespace ReservationSystem.Shared.DTO
+{
+    public class PaginatedResult<T>
+    {
+        public List<T> Data { get; set; }
+        public int TotalCount { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}

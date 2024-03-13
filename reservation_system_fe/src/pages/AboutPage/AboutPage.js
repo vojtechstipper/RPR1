0de746699a7d64901786ab87c488505ce4dc1e96
Martin  Šašinka <93054857+VelkaSunka@users.noreply.github.com>
2024-03-13 18:06:42 +0100

    AboutPage, OrderBanner

--- reservation_system_fe/src/pages/AboutPage/AboutPage.js
+++ reservation_system_fe/src/pages/AboutPage/AboutPage.js
@@ -1,13 +1,56 @@
 import React from 'react';
-import Typography from "@mui/material/Typography";
+import { Container, Typography, Grid,} from '@mui/material';
+import ImageGallery from 'react-image-gallery';
+import Coverphoto from '../../static/img/coverphoto.jpg';
+import 'react-image-gallery/styles/css/image-gallery.css';
+import './components/AboutPage.css';
 
+// Import your images
+//import Photo1 from '../../static/img/photo1.jpg';
+//import Photo2 from '../../static/img/photo2.jpg';
+//import Photo3 from '../../static/img/photo3.jpg';
 
-function AboutPage() {
-    return (
-        <div>
-            <Typography>About</Typography>
-        </ div>
-    )
-}
+import 'react-image-gallery/styles/css/image-gallery.css';
 
-export default AboutPage;
+const AboutUs = () => {
+    const aboutPageText = process.env.REACT_APP_ABOUTPAGE.replace(/\\n/g, '\n');
+  const images = [
+    {
+      original: Coverphoto,
+      thumbnail: Coverphoto,
+    },
+    {
+      original: Coverphoto,
+      thumbnail: Coverphoto,
+    },
+    {
+      original: Coverphoto,
+      thumbnail: Coverphoto,
+    },
+  ];
+
+  return (
+    <Container maxWidth="xl">
+      <Grid container spacing={3} alignItems="center" sx={{ padding: '20px',}}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="h3" component="h2" gutterBottom textAlign="left">
+            O nás
+          </Typography>
+          <Typography variant="body1" paragraph>
+            {aboutPageText.split('\n').map((paragraph, index) => (
+                <React.Fragment key={index}>
+                {paragraph}
+                <br />
+                </React.Fragment>
+            ))}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6}>
+            <ImageGallery items={images} autoPlay={true} showPlayButton={false} slideInterval={5000} infinite={true} showThumbnails={false} />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default AboutUs;

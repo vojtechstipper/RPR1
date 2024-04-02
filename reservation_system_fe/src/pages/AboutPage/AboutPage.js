fa81253f7467113e0adacfbbd5f50a285ae5491b
Martin  Šašinka <93054857+VelkaSunka@users.noreply.github.com>
2024-04-02 12:28:02 +0200

    Deleted ImageGallery
    added just one static photo

--- reservation_system_fe/src/pages/AboutPage/AboutPage.js
+++ reservation_system_fe/src/pages/AboutPage/AboutPage.js
@@ -1,56 +1,32 @@
 import React from 'react';
-import { Container, Typography, Grid,} from '@mui/material';
-import ImageGallery from 'react-image-gallery';
-import Coverphoto from '../../static/img/coverphoto.jpg';
-import 'react-image-gallery/styles/css/image-gallery.css';
-import './components/AboutPage.css';
-
-// Import your images
-//import Photo1 from '../../static/img/photo1.jpg';
-//import Photo2 from '../../static/img/photo2.jpg';
-//import Photo3 from '../../static/img/photo3.jpg';
-
-import 'react-image-gallery/styles/css/image-gallery.css';
+import { Container, Typography, Grid } from '@mui/material';
+import CoverPhoto from '../../static/img/coverphoto.jpg';
 
 const AboutUs = () => {
     const aboutPageText = process.env.REACT_APP_ABOUTPAGE.replace(/\\n/g, '\n');
-  const images = [
-    {
-      original: Coverphoto,
-      thumbnail: Coverphoto,
-    },
-    {
-      original: Coverphoto,
-      thumbnail: Coverphoto,
-    },
-    {
-      original: Coverphoto,
-      thumbnail: Coverphoto,
-    },
-  ];
 
-  return (
-    <Container maxWidth="xl">
-      <Grid container spacing={3} alignItems="center" sx={{ padding: '20px',}}>
-        <Grid item xs={12} md={6}>
-          <Typography variant="h3" component="h2" gutterBottom textAlign="left">
-            O nás
-          </Typography>
-          <Typography variant="body1" paragraph>
-            {aboutPageText.split('\n').map((paragraph, index) => (
-                <React.Fragment key={index}>
-                {paragraph}
-                <br />
-                </React.Fragment>
-            ))}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} md={6}>
-            <ImageGallery items={images} autoPlay={true} showPlayButton={false} slideInterval={5000} infinite={true} showThumbnails={false} />
-        </Grid>
-      </Grid>
-    </Container>
-  );
+    return (
+        <Container maxWidth="xl">
+            <Grid container spacing={3} alignItems="center" sx={{ padding: '20px' }}>
+                <Grid item xs={12} md={6}>
+                    <Typography variant="h3" component="h2" gutterBottom textAlign="left">
+                        O nás
+                    </Typography>
+                    <Typography variant="body1" paragraph>
+                        {aboutPageText.split('\n').map((paragraph, index) => (
+                            <React.Fragment key={index}>
+                                {paragraph}
+                                <br />
+                            </React.Fragment>
+                        ))}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <img src={CoverPhoto} alt="Cover" style={{ width: '100%', height: 'auto' }} />
+                </Grid>
+            </Grid>
+        </Container>
+    );
 };
 
 export default AboutUs;
